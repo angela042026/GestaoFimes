f36b5757a5e966bcd0bc7b389427337eb41368df
Angela <angela.costa.233225838@msft.cesae.pt>
2026-05-27 14:45:35 +0100

    inseri a class filmes

--- GestaoFilmes.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GestaoFilmes.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ GestaoFilmes.Domain/Entities/Filmes.cs
@@ -0,0 +1,20 @@
+﻿namespace GestaoFilmes.Domain.Entities
+{
+    public class Filmes
+    
+        {
+            //modelo de dados do sistema
+
+            public int Id { get; set; }
+            public string Título { get; set; }
+            public DateTime Ano { get; set; }
+            public string Língua { get; set; }
+                       
+            public Filmes()
+            {
+                Título = string.Empty;//defenir como vazio para evitar null
+            }
+        
+    }
+}
+
